f9e085820ff05eafe5bec9afad80bc4954dd701a
Vladimir Vasilenko <9319669@gmail.com>
2023-10-01 22:49:53 +0300

    Замечания от 29.09.23 исправлены

--- src/test/java/ru/netology/DeliveryCardPositiveTest.java
+++ src/test/java/ru/netology/DeliveryCardPositiveTest.java
@@ -1,11 +1,14 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.conditions.Visible;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
@@ -15,16 +18,25 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class DeliveryCardPositiveTest {
 
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+
     @Test
     void shouldOpenSours() {
         open("http://localhost:9999");
         $("[data-test-id='city'] .input__control").setValue("Санкт-Петербург");
-        $("[data-test-id='date'] .input__control").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        $("[data-test-id='date'] .input__control").setValue("01.10.2023");
+        $("[data-test-id='date'] .input__control")
+                .sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        String planningDate = generateDate(5);
+        $("[data-test-id='date'] .input__control").setValue(planningDate);
         $("[data-test-id='name'] .input__control").setValue("Владимир Иванов");
         $("[data-test-id='phone'] .input__control").setValue("+79210000000");
         $("[data-test-id='agreement'] .checkbox__box").click();
         $(".button").click();
         $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text(planningDate)).shouldBe(Condition.visible);
     }
 }
